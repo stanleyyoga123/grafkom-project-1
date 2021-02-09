05abaf42e2bbd11ecc97e32b98a878524fc78b98
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-02-09 07:47:04 +0700

    chore: delete unnecessary file

--- public/static/js/events/download.mjs
+++
@@ -1,9 +0,0 @@
-export function download(content, fileName, contentType) {
-    var a = document.createElement("a");
-    var file = new Blob([content], {type: contentType});
-    a.href = URL.createObjectURL(file);
-    a.download = fileName;
-    a.click();
-}
-
-// download(jsonData, 'json.txt', 'text/json');
--- public/static/js/events/upload.mjs
+++
@@ -1,3 +0,0 @@
-export function upload(){
-    alert(123)
-}
